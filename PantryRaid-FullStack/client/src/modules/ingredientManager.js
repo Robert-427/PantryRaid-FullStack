d7043f64c1eb786105ba59c07ca80015026fdefb
Robert Parker <rober.m.parker.j@gmail.com>
2022-10-20 10:54:02 -0500

    Added Managers to frontend

--- PantryRaid-FullStack/client/src/modules/ingredientManager.js
+++ PantryRaid-FullStack/client/src/modules/ingredientManager.js
@@ -5,12 +5,37 @@ export const getAllIngredientsFromApi = () => {
     .then((res) => res.json())
 };
 
-export const getAllIngredientsByRecipeFromApi = (recipeId) => {
-    return fetch(baseUrl+`GetByRecipe/${recipeId}`)
+export const getIngredientsByUserFromApi = (firebaseUserId) => {
+    return fetch(baseUrl+`/GetByUser/${firebaseUserId}`)
     .then((res) => res.json())
-};
+}
 
 export const getIngredientByIdFromApi = (ingredientId) => {
     return fetch(baseUrl+`/${ingredientId}`)
     .then((res) => res.json())
+};
+
+export const getAllIngredientsByRecipeFromApi = (recipeId) => {
+    return fetch(baseUrl+`/GetByRecipe/${recipeId}`)
+    .then((res) => res.json())
+};
+
+export const addNewIngredientToApi = (ingredient) => {
+    return fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(ingredient),
+    });
+};
+
+export const updateIngredintInApi = (ingredient) => {
+    return fetch(baseUrl+`/${ingredient.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(ingredient),
+    });
 };
